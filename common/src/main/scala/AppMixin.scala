10711b046455debc77b4a5d0b2b219e7260702f6
Hanns Holger Rutz <contact@sciss.de>
2016-03-09 21:36:04 +0100

    ...

--- common/src/main/scala/AppMixin.scala
+++ common/src/main/scala/AppMixin.scala
@@ -1,6 +1,8 @@
-import java.awt.Color
+import java.awt.{Color, Graphics}
+import javax.swing.JPanel
+import javax.swing.border.Border
 
-import scala.swing.{BorderPanel, BoxPanel, Button, FlowPanel, Frame, Label, MainFrame, Orientation, Swing}
+import scala.swing.{Component, BorderPanel, BoxPanel, Button, FlowPanel, Frame, Label, MainFrame, Orientation, Swing}
 
 trait AppMixin { app =>
   def title: String
@@ -11,7 +13,25 @@ trait AppMixin { app =>
     val p1  = new BoxPanel(Orientation.Horizontal) {
       contents ++= Seq(gg1, gg2, Swing.HGlue)
     }
-    val p2 = new FlowPanel(new Label("___________________"), Button("Baz")())
+    val gg3 = new Label("___________________")
+    val gg4 = Button("Baz")()
+
+    val p2j = new JPanel(new java.awt.FlowLayout) {
+      override def paint(g: Graphics): Unit = {
+        // overridden to allow breakpoint for debugger
+        println("paint")
+        super.paint(g)
+      }
+
+      override def setBorder(border: Border): Unit = {
+        println("setBorder")
+        super.setBorder(border)
+      }
+    }
+    p2j.add(gg3.peer)
+    p2j.add(gg4.peer)
+
+    val p2 = Component.wrap(p2j)
     p2.border = Swing.TitledBorder(Swing.LineBorder(Color.blue), "Title")
     val p3 = new BorderPanel {
       layout(p1) = BorderPanel.Position.North
