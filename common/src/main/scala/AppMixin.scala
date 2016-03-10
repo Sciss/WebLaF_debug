8ab11691a59d66f8ab592596c008d7fbb9b6dd55
Hanns Holger Rutz <contact@sciss.de>
2016-03-10 15:06:24 +0100

    case for wrong box-layout / hglue

--- common/src/main/scala/AppMixin.scala
+++ common/src/main/scala/AppMixin.scala
@@ -1,45 +1,27 @@
-import java.awt.{Color, Graphics}
-import javax.swing.JPanel
-import javax.swing.border.Border
-
-import scala.swing.{Component, BorderPanel, BoxPanel, Button, FlowPanel, Frame, Label, MainFrame, Orientation, Swing}
+import scala.swing.Swing._
+import scala.swing._
 
 trait AppMixin { app =>
   def title: String
 
   lazy val top: Frame = {
-    val gg1 = Button("Foo")()
-    val gg2 = Button("Bar")()
-    val p1  = new BoxPanel(Orientation.Horizontal) {
-      contents ++= Seq(gg1, gg2, Swing.HGlue)
+    val topPane = new BoxPanel(Orientation.Horizontal) {
+      contents += new Button("Start")
+      contents += HGlue
     }
-    val gg3 = new Label("___________________")
-    val gg4 = Button("Baz")()
 
-    val p2j = new JPanel(new java.awt.FlowLayout) {
-      override def paint(g: Graphics): Unit = {
-        // overridden to allow breakpoint for debugger
-        println("paint")
-        super.paint(g)
-      }
-
-      override def setBorder(border: Border): Unit = {
-        println("setBorder")
-        super.setBorder(border)
-      }
+    val bottomPane = new BoxPanel(Orientation.Horizontal) {
+      contents += new Label("-------------- Audio Status --------------")
+      contents += HGlue
     }
-    p2j.add(gg3.peer)
-    p2j.add(gg4.peer)
 
-    val p2 = Component.wrap(p2j)
-    p2.border = Swing.TitledBorder(Swing.LineBorder(Color.blue), "Title")
-    val p3 = new BorderPanel {
-      layout(p1) = BorderPanel.Position.North
-      layout(p2) = BorderPanel.Position.Center
-    }
+    val boxPane = new BoxPanel(Orientation.Vertical)
+    boxPane.contents += topPane
+    boxPane.contents += bottomPane
+
     new MainFrame {
       title     = app.title
-      contents  = p3
+      contents  = boxPane
     }
   }
 }
